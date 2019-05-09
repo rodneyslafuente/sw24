b1eee44f7c6a7714d2a3e109b4c8d3136d0073bc
Rodney Lafuente Mercado <rodneyslafuente@gmail.com>
2019-05-09 18:24:00 -0400

    implemented openlayers

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 import os
 
-from cs50 import SQL
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
-from flask_session import Session
+from flask import Flask, flash, jsonify, redirect, render_template, request
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, lookup
+
+import json
 
 # Configure application
 app = Flask(__name__)
@@ -23,24 +23,11 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
 @app.route("/")
-@login_required
 def index():
 
-    return render_template("index.html")
+        return render_template("test.html")
 
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-
-    # Render buy.html
-    return render_template("buy.html")
 
 def errorhandler(e):
     """Handle error"""
--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 import requests
 import urllib.parse
 
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, request
 from functools import wraps
 
 
@@ -18,20 +18,6 @@ def apology(message, code=400):
             s = s.replace(old, new)
         return s
     return render_template("apology.html", top=code, bottom=escape(message)), code
-
-
-def login_required(f):
-    """
-    Decorate routes to require login.
-
-    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
-    """
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/login")
-        return f(*args, **kwargs)
-    return decorated_function
 
 
 def lookup(symbol):
@@ -55,6 +41,3 @@ def lookup(symbol):
     except (KeyError, TypeError, ValueError):
         return None
 
-def usd(value):
-    """Format value as USD."""
-    return f"${value:,.2f}"
